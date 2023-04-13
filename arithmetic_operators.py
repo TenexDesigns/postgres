f1ae7c83721115ba7ee49ea25a4694caeb5eab2d
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-04-13 14:41:08 +0300

    Create arithmetic_operators.py

---
+++ arithmetic_operators.py
@@ -0,0 +1,62 @@
+PostgreSQL supports a variety of arithmetic operators that can be used to perform mathematical operations on numeric data types.
+Here are the most commonly used arithmetic operators in PostgreSQL:
+
+Addition: +
+  
+  SELECT 2 + 3;  -- result: 5
+
+Subtraction: -
+  
+  SELECT 5 - 3;  -- result: 2
+    
+Multiplication: *
+  
+  SELECT 2 * 3;  -- result: 6
+    
+Division: /
+  
+  SELECT 10 / 2;  -- result: 5
+
+Modulus (remainder): %
+  
+  SELECT 10 % 3;  -- result: 1
+
+Exponentiation: ^ or **
+  
+  SELECT 2 ^ 3;    -- result: 8
+  SELECT 2 ** 3;   -- result: 8
+
+
+These arithmetic operators can be used with numeric data types such as integer, bigint, decimal, numeric, float, real, etc. 
+In addition, you can use parentheses to group arithmetic expressions to ensure that they are evaluated in the correct order.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+...
