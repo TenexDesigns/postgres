b0e93b76321c6e1a83f44f38f767a3b1e395b2c2
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-04-13 17:01:37 +0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -41,45 +41,103 @@ By using these functions, you can manipulate and summarize data in powerful and 
 
 
 
+CREATING FUNCTIONS
+***********************************************************************************************************************************************************
+Functions in PostgreSQL are named blocks of code that perform a specific task.
+They can be used to simplify complex operations, improve query performance, and promote code reuse. 
+In PostgreSQL, functions are created using the CREATE FUNCTION statement.
+
+Here is the basic syntax for creating a function in PostgreSQL:
+  
+  
+
+CREATE FUNCTION function_name (argument_list)
+RETURNS return_type AS $$
+BEGIN
+  -- Function body goes here
+END;
+$$ LANGUAGE plpgsql;
+
+
+
+
+
+Lets break down each part of the syntax:
+
+CREATE FUNCTION: This statement begins the function creation process.
+
+function_name: This is the name you give to the function.
+
+argument_list: This is a comma-separated list of arguments that the function will take. Each argument has a name and a data type.
+
+RETURNS: This specifies the data type that the function will return.
+
+return_type: This is the data type that the function will return.
+
+$$: This is the dollar-quoted string syntax that is used to enclose the function body.
+
+BEGIN: This keyword marks the beginning of the function body.
+
+END: This keyword marks the end of the function body.
+
+LANGUAGE plpgsql: This specifies the language that the function is written in.
+PostgreSQL supports several different languages, but plpgsql is the most commonly used language for writing functions.
+
+
+
+
+Once you have created a function, you can call it from within your SQL code using the function name and any required arguments. For example:
+
+SELECT function_name(argument1, argument2);
+
+
+Overall, functions are a powerful feature of PostgreSQL that allow you to create custom,
+reusable code blocks that can simplify complex operations and improve query performance.
 
 
 
 
 
 
+Here is an example of a scalar function that takes a string as input and returns the length of the string:
+
+
+CREATE FUNCTION string_length(input_string text)
+RETURNS integer AS $$
+BEGIN
+  RETURN length(input_string);
+END;
+$$ LANGUAGE plpgsql;
 
 
 
+HERE IS ANOTHER EXAMPLE
+******************************************************************************************************************************
+Sure, heres an example of a simple function in PostgreSQL that takes two integers as input, adds them together, and returns the result:
+
+
+CREATE FUNCTION add_numbers(x INT, y INT)
+RETURNS INT AS $$
+BEGIN
+  RETURN x + y;
+END;
+$$ LANGUAGE plpgsql;
 
 
 
+In this example, the function is called add_numbers and takes two arguments of type INT. 
+The RETURNS clause specifies that the function will return an integer value.
+The function body simply adds the two input values together and returns the result using the RETURN keyword.
 
 
+You can call this function like this:
 
+SELECT add_numbers(2, 3); -- Output: 5
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  
+  
+  This will return the value 5, which is the result of adding 2 and 3 together using the add_numbers function.
+  
 
 
 
