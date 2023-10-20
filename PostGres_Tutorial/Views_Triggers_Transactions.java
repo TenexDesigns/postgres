a2e22707565374e2433c39a054d8af79bd4e519c
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-10-20 15:13:18 +0300

    Update Views_Triggers_Transactions.java

--- PostGres_Tutorial/Views_Triggers_Transactions.java
+++ PostGres_Tutorial/Views_Triggers_Transactions.java
@@ -122,3 +122,131 @@ These code samples illustrate how to work with views, materialized views, trigge
 
 
   ....
+    ------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+    Certainly, let's explore each of these topics with code samples:
+
+**1. Views:**
+A view is a virtual table based on the result of a SQL query. Views can simplify complex queries and provide a logical layer over the underlying tables.
+
+Creating a View:
+```sql
+-- Creating a view to retrieve information about students
+CREATE VIEW student_view AS
+SELECT id, name, age
+FROM students
+WHERE status = 'active';
+
+-- Querying the view
+SELECT * FROM student_view;
+```
+
+**2. Materialized Views:**
+A materialized view is a physical copy of the query result that is updated periodically. It can significantly improve query performance by storing precomputed results.
+
+Creating a Materialized View:
+```sql
+-- Creating a materialized view to store the average age of students
+CREATE MATERIALIZED VIEW avg_student_age AS
+SELECT AVG(age) as avg_age
+FROM students
+WHERE status = 'active';
+
+-- Refresh the materialized view when needed
+REFRESH MATERIALIZED VIEW avg_student_age;
+
+-- Querying the materialized view
+SELECT * FROM avg_student_age;
+```
+
+**3. Triggers:**
+Triggers are database functions that are automatically invoked when a specific event occurs in the database.
+
+Creating a Trigger:
+```sql
+-- Creating a trigger to update a timestamp when a student record is modified
+CREATE OR REPLACE FUNCTION update_timestamp()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.updated_at = NOW();
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Attaching the trigger to the students table
+CREATE TRIGGER update_student_timestamp
+BEFORE UPDATE ON students
+FOR EACH ROW
+EXECUTE FUNCTION update_timestamp();
+```
+
+**4. Functions and Procedures:**
+User-defined functions and procedures can be written in PL/pgSQL or other supported languages.
+
+Creating a Function:
+```sql
+-- Creating a PL/pgSQL function to calculate the square of a number
+CREATE OR REPLACE FUNCTION square(num numeric)
+RETURNS numeric AS $$
+BEGIN
+    RETURN num * num;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Using the function
+SELECT square(5); -- Returns 25
+```
+
+**5. Transactions:**
+PostgreSQL supports transactions to ensure the integrity of data and consistency of the database.
+
+Using Transactions:
+```sql
+-- Start a transaction
+BEGIN;
+
+-- Insert data
+INSERT INTO students (name, age, status) VALUES ('Alice', 22, 'active');
+
+-- Update data
+UPDATE students SET age = 23 WHERE name = 'Alice';
+
+-- Commit the transaction to save changes
+COMMIT;
+
+-- Rollback the transaction to undo changes
+ROLLBACK;
+```
+
+**6. Savepoints:**
+Savepoints allow you to set points within a transaction to which you can later roll back.
+
+Using Savepoints:
+```sql
+-- Start a transaction
+BEGIN;
+
+-- Insert data
+INSERT INTO students (name, age, status) VALUES ('Bob', 25, 'active');
+
+-- Set a savepoint
+SAVEPOINT my_savepoint;
+
+-- Update data
+UPDATE students SET age = 26 WHERE name = 'Bob';
+
+-- Roll back to the savepoint
+ROLLBACK TO my_savepoint;
+
+-- Commit the transaction to save changes
+COMMIT;
+```
+
+These code samples illustrate how to work with views, materialized views, triggers, functions, procedures, transactions, and savepoints in PostgreSQL. You can adapt and extend these examples to meet your specific database needs.
+
+
+
+
+
+    .....
